9d4a97c3a6759950447a00fbf48b756793c4cec5
SomaTakata <somatakata@gmail.com>
2024-02-12 20:20:39 +0900

    change auth.config.js

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
     providers: [
       {
-        domain: "https://your-issuer-url.clerk.accounts.dev/",
+        domain: "https://settled-arachnid-21.clerk.accounts.dev",
         applicationID: "convex",
       },
     ]
