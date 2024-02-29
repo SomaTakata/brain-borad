b379a33ba7c4d37353c270c1c3388e1f1d46ea53
SomaTakata <somatakata@gmail.com>
2024-02-29 21:45:14 +0900

    Navbar Layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import SideBar from "./_components/sidebar";
 import OrgSideBar from "./_components/org-sidebar";
+import Navbar from "./_components/navbar";
 
 interface DashboardLayoutProps {
   children: React.ReactNode;
@@ -13,7 +14,10 @@ const DashboardLayout = ({ children }: DashboardLayoutProps) => {
       <div className="pl-[60px] h-full">
         <div className="flex gap-x-3 h-full">
           <OrgSideBar />
-          <div className="h-full flex-1">{children}</div>
+          <div className="h-full flex-1">
+            <Navbar />
+            {children}
+          </div>
         </div>
       </div>
     </main>
