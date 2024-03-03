bd811581adbba754182583c63866c9fe0945fb82
SomaTakata <somatakata@gmail.com>
2024-03-03 21:24:53 +0900

    api mutation のhooks作成 return mutate,pending

---
+++ hooks/use-api-mutation.ts
@@ -0,0 +1,24 @@
+import { useMutation } from "convex/react";
+import { useState } from "react";
+
+export const useApiMutation = (mutationFunction: any) => {
+  const [pending, setPending] = useState(false);
+  const apiMutaion = useMutation(mutationFunction);
+
+  const mutate = (payload: any) => {
+    setPending(true);
+    return apiMutaion(payload)
+      .finally(() => setPending(false))
+      .then((result) => {
+        return result;
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+
+  return {
+    mutate,
+    pending,
+  };
+};
