3299ce446e670ba299a383cd7afd3734546b7f9d
SomaTakata <somatakata@gmail.com>
2024-03-03 11:23:03 +0900

    svgの追加

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const DashBoardPage = () => {
-  return <div>DashBoard Root Page</div>;
+  return (
+    <div
+      className="
+   bg-red-500 flex-1 h-[calc(100%-80px)] p-6"
+    >
+      DashBoard Root Page
+    </div>
+  );
 };
 
 export default DashBoardPage;
